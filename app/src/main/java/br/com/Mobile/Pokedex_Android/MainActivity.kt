1ff53b1cd162cc1fd938b2805dcde5c09c728c95
MizaelJR17 <mizael.jxjunior@aluno.educacao.pe.gov.br>
2022-10-01 16:31:09 -0300

    Criação da Tela de Pokemons

---
+++ app/src/main/java/br/com/Mobile/Pokedex_Android/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.Mobile.Pokedex_Android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pokemon_item)
+    }
+}
